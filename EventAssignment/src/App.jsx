c253363c125b7d7d509196ffac0ddceaf70f43a4
Nishayadav1 <nishayadav22@navgurukul.org>
2025-02-12 10:15:55 +0530

    event assignment compeleted

--- EventAssignment/src/App.jsx
+++ EventAssignment/src/App.jsx
@@ -1,16 +1,33 @@
-import './App.css'
+import './App.css';
 
-function App() {
-let count;
-  return (
-    <>
-      <h1>Counter without state</h1>
-      <h2>{count}</h2>
-      <button style={{backgroundColor:'green', marginRight:"20px"}}
-            onClick=()=>{count+=1}>Increment +1</button>
-      <button style={{backgroundColor:'orange'}}>Decrement -1</button>
-    </>
-  )
+let counter = 0;
+
+function eventHandleAdd() {
+  counter += 1;
+  document.getElementById('counter').innerText = `Counter: ${counter}`;
+  document.getElementById('dec').style.display = 'inline-block'; 
 }
 
-export default App
+function eventHandleMins() {
+  if (counter > 0) {
+    counter -= 1;
+    document.getElementById('counter').innerText = `Counter: ${counter}`;
+  }
+  
+  if (counter === 0) {
+    document.getElementById('dec').style.display = 'none'; 
+  }
+}
+
+function App() {
+  return (
+    <>
+      <h1>Counter without State</h1>
+      <h2 id="counter">Counter: {counter}</h2>
+      <button onClick={eventHandleAdd}>Increment</button>
+      <button onClick={eventHandleMins} id="dec" style={{ display: 'none' }}>Decrement</button>
+    </>
+  );
+}
+
+export default App;
